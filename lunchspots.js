2aa3d23a73e055e49c77071dae8734a6fdf9dce8
Thom Boyer <thom@boyers.org>
2013-08-03 17:18:00 -0600

    Made lunchspot.js jslint-clean

--- lunchspots.js
+++ lunchspots.js
@@ -1,44 +1,69 @@
-// Set up a collection to contain restaurant location information. On the 
-// server, it is backed by a MongoDB collection named "venues".
+/* vim: set autoindent tabstop=2 shiftwidth=2 expandtab: */
 
-Venues = new Meteor.Collection("venues");
+(function () {
+  "use strict";
 
-if (Meteor.isClient) {
-  Template.lunchspots.venues = function () {
-    return Venues.find({}, {sort: {score: -1, name: 1}});
-  };
+  /* jslint and global directives cannot have space before the keyword. Ugh. */
+  /*globals Meteor, Template, Session, Random */
+  /*jslint browser: true, devel: true, nomen: true, plusplus: true */
+  /*
+   * Explanation of jslint options:
+   *   browser: true 
+   *     assume the standard browser global variables
+   *   devel: true
+   *     assume standard globals useful for development (like 'console')
+   *   nomen: true
+   *     suppresses complaints about identifiers with leading and/or
+   *     trailing underscores
+   *   plusplus: true
+   *     suppresses complaints about ++ and --
+   */
 
-  Template.lunchspots.selected_name = function () {
-    var venue = Venues.findOne(Session.get("selected_player"));
-    return venue && venue.name;
-  };
+  // Set up a collection to contain restaurant location information.
+  // On the server, it is backed by a MongoDB collection named "venues".
+  var Venues = new Meteor.Collection("venues");
+        
+  if (Meteor.isClient) {
+    Template.lunchspots.venues = function () {
+      return Venues.find({}, {sort: {score: -1, name: 1}});
+    };
 
-  Template.venue.selected = function () {
-    return Session.equals("selected_player", this._id) ? "selected" : '';
-  };
+    Template.lunchspots.selected_name = function () {
+      var venue = Venues.findOne(Session.get("selected_player"));
+      return venue && venue.name;
+    };
 
-  Template.lunchspots.events({
-    'click input.inc': function () {
-      Venues.update(Session.get("selected_player"), {$inc: {score: 5}});
-    }
-  });
+    Template.venue.selected = function () {
+      return Session.equals("selected_player", this._id) ? "selected" : '';
+    };
 
-  Template.venue.events({
-    'click': function () {
-      Session.set("selected_player", this._id);
-    }
-  });
-}
+    Template.lunchspots.events({
+      'click input.inc': function () {
+        Venues.update(Session.get("selected_player"), {$inc: {score: 5}});
+      }
+    });
 
-// On server startup, create some venues if the database is empty.
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    if (Venues.find().count() === 0) {
-      var names = ["Wendy's",
-                   "Buffalo Wild Wings",
-                   "Jason's Deli"];
-      for (var i = 0; i < names.length; i++)
-        Venues.insert({name: names[i], score: Math.floor(Random.fraction()*10)*5});
-    }
-  });
-}
+    Template.venue.events({
+      'click': function () {
+        Session.set("selected_player", this._id);
+      }
+    });
+  }
+
+  // On server startup, create some venues if the database is empty.
+  if (Meteor.isServer) {
+    Meteor.startup(function () {
+      if (Venues.find().count() === 0) {
+        var i, random_score, names;
+        names = ["Wendy's",
+                 "Buffalo Wild Wings",
+                 "Jason's Deli"];
+        for (i = 0; i < names.length; i++) {
+          random_score = Math.floor(Random.fraction() * 10) * 5;
+          Venues.insert({name: names[i], score: random_score});
+        }
+      }
+    });
+  }
+
+}());
