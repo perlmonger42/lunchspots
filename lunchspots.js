32e7063803391facf9b002a680e8ed8273712919
Thom Boyer <thom@boyers.org>
2013-08-18 19:05:04 -0600

    Added 'Recalculate Scores', changed rating scale.
    There is now a link in the upper right corner that allows you to
    recalculate all the venue scores. A venue's score is not the sum of all
    votes, as it was previously. It is, instead, the average of all the
    existing ratings for that venue.
    
    The user-applied ratings are now 0, 2, 4, 6, 8, 10, or X. The X value
    means "no rating at all", which means that the user contributes nothing
    to the sum or count used in computing the venue's average.

--- lunchspots.js
+++ lunchspots.js
@@ -16,20 +16,70 @@
       return Session.equals("selected_venue", this._id) ? "selected" : '';
     };
 
+    Template.venue.roundedScore = function () {
+      return this.score.toFixed(1);
+    };
+
+    Template.venue.rawScore = function () {
+      return computeScore(this.ratingSum, this.ratingCount);
+    };
+
     Template.venue.ratingStatus = function (aRating) {
       var selected = Session.equals("selected_venue", this._id);
       if (!selected) { return 'inactiveRating' }
       var r = Ratings.findOne({user: Meteor.userId(), venue: this._id});
-      if (!r) { return 'newRating' }
+      if (!r) {
+        return aRating === 5 ? 'selectedRating' : 'newRating';
+      }
       return r.value === aRating ? 'selectedRating' : 'newRating';
     };
 
+    Template.controls.events({
+      'click #recalc': function (/*theEvent, controlsTemplate*/) {
+        /*var context = this;*/
+        var stats, ratings, venues;
+        stats = { };
+        ratings = Ratings.find({}).fetch();
+        venues = Venues.find({}).fetch();
+        _.each(venues, function (venue) {
+          var venueId = venue._id, venueStats = stats[venueId];
+          if (!venueStats) {
+            venueStats = stats[venueId] = { sum: 0, count: 0 };
+          }
+        });
+        _.each(ratings, function (rating) {
+          var venueId, venueStats;
+          venueId = rating.venue;
+          venueStats = stats[venueId];
+          if (!venueStats) {
+            venueStats = stats[venueId] = { sum: 0, count: 0 };
+          }
+          venueStats.sum += rating.value;
+          venueStats.count += 1;
+        });
+        _.each(stats, function (venueStats, venueId) {
+          var venueScore = computeScore(venueStats.sum, venueStats.count);
+          Venues.update(venueId, {$set: {
+            score:       venueScore,
+            ratingSum:   venueStats.sum,
+            ratingCount: venueStats.count
+          }});
+        });
+      }
+    });
+
     Template.lunchspots.events({
-      'click span.newRating': function (theEvent, theTemplate) {
-        var theVenue = this;
-        if (theTemplate || theVenue) { /* suppress unused var warning */ }
-        var value = parseInt(theEvent.currentTarget.innerText, 10);
-        rateVenue(Meteor.userId(), Session.get("selected_venue"), value);
+      'click span.newRating': function (theEvent /*, lunchspotsTemplate*/) {
+        var theVenue = this; // `this` is bound to the current venue. Why? 
+        // When event methods are dispatched, Meteor binds `this` to "the
+        // current context".  Our HTML has a loop, `{{#each venues}}`,
+        // which expands its body once per venue, setting the current
+        // context to the current venue.  And since the venue-ratings
+        // buttons are generated inside the `venue` template, which is
+        // inside the `{{#each venues}}` loop, we get the venue in `this`.
+        var value = theEvent.currentTarget.innerText;
+        if (value === 'X') { value = ''; /* no rating at all */ }
+        rateVenue(Meteor.userId(), theVenue._id, value);
       },
       'click input.add': function () {
         addVenue();
@@ -64,13 +114,16 @@
         for (i = 0; i < venues.length; i++) {
           //random_score = Math.floor(Random.fraction() * 10) * 5;
           //random_score = Math.floor(Random.fraction() * 10);
-          random_score = 0;
+          //random_score = 0;
+          random_score = (Random.fraction() * 10);
           venue = venues[i];
           Venues.insert({
             name:    venue[0],
             address: venue[1],
             city:    venue[2],
-            score:   random_score
+            score:   random_score,
+            ratingSum:   0,
+            ratingCount: 0
           });
         }
       }
@@ -78,20 +131,59 @@
   }
 
   function rateVenue(userId, venueId, newRating) {
-    var delta, oldRating;
-    var r = Ratings.find({user: userId, venue: venueId}, {limit: 1}).fetch();
-    if (r.length === 0) {
-      oldRating = 0;
-      r = {user: userId, venue: venueId, value: newRating};
-      Ratings.insert({user: userId, venue: venueId, value: newRating});
-    } else {
-      r = r[0];
-      oldRating = r.value;
-      Ratings.update(r._id, {$set: {value: newRating}});
+    if (newRating !== '') {
+      newRating = parseInt(newRating, 10);
     }
-    delta = newRating - oldRating;
-    if (delta !== 0) {
-      Venues.update(venueId, {$inc: {score: delta}});
+    var deltaSum, deltaCount, oldRating;
+    var r = Ratings.findOne({user: userId, venue: venueId});
+    if (r && typeof r.value !== 'number') {
+      // This shouldn't happen, but clean it up if it does.
+      Ratings.remove(r._id);
+    }
+
+    if (!r) {
+      // Previously, userId had no rating for venueId.
+      oldRating = '';
+      if (newRating === '') {
+        // Nothing to do: there was no rating before and no rating now.
+        deltaSum = 0;
+        deltaCount = 0;
+      } else {
+        // Insert a new rating.
+        deltaSum = newRating;
+        deltaCount = 1;
+        Ratings.insert({user: userId, venue: venueId, value: newRating});
+      }
+    } else {
+      // Previouly, userId had a rating for venueId.
+      oldRating = r.value;
+      if (newRating === '') {
+        // Remove the existing rating.
+        deltaSum = -oldRating;
+        deltaCount = -1;
+        Ratings.remove(r._id);
+      } else {
+        // Update the existing rating.
+        deltaSum = newRating - oldRating;
+        deltaCount = 0;
+        Ratings.update(r._id, {$set: {value: newRating}});
+      }
+    }
+
+    // Apply any changes to the venue score.
+    if (deltaSum !== 0 || deltaCount !== 0) {
+      var v = Venues.findOne(venueId);
+      if (v) {
+        var sum = v.ratingSum + deltaSum;
+        var count = v.ratingCount + deltaCount;
+        var score = computeScore(sum, count);
+
+        Venues.update(venueId, {$set: {
+          score:       score,
+          ratingSum:   sum,
+          ratingCount: count
+        }});
+      }
     }
   }
 
@@ -126,5 +218,10 @@
     );
   }
 
+  function computeScore(sum, count) {
+    // Venues that haven't been scored should get a score of 5, not  NaN.
+    return count === 0 ? 5 : sum / count;
+  }
+
 })();
-/* vim: set autoindent tabstop=2 shiftwidth=2 expandtab wrapmargin=76: */
+/* vim: set autoindent tabstop=2 shiftwidth=2 expandtab wrapmargin=6: */
